2280838626ccbe18260ff177f886b943508e60a4
Christian Salazar <salazar3148@gmail.com>
2023-01-19 17:13:34 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     
     res.header('Access-Control-Allow-Headers', '*');
+
+    res.header('Access-Control-Allow-Methods', '*');
+
     next();
 })
 
